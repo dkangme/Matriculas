add9be949b4310bebfa64f54624b4e387ff06587
Dennis Kangme <dkangme@k-group.cl>
2016-04-11 07:33:27 -0300

    v.2.

---
+++ undo.sh
@@ -0,0 +1,51 @@
+	git reset HEAD  AppAuditMatriculas/2010_alumnos.csv
+	git reset HEAD  AppAuditMatriculas/2010_historial.csv
+	git reset HEAD  AppAuditMatriculas/2011_alumnos.csv
+	git reset HEAD  AppAuditMatriculas/2011_historial.csv
+	git reset HEAD  AppAuditMatriculas/ALUMNOS_2010.csv
+	git reset HEAD  AppAuditMatriculas/ALUMNOS_2011.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_G1_LOADER.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_2013_IES_40 2013.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_2014_POSGRADOYPOSTITULO_40 2014.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_2014_PREGRADO_40 2014.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_2015_POSTGRADOYPOSTITULO_40 2015.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_2015_PREGRADO_40 2015.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_40 2011.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_POSTGRADO_Y_POSTITULO_2013_IES_40 2013.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_POSTGRADO_y_POSTITULO_40 2012.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G1/A_MAT_PREGRADO_40 2012.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G2/A_BORRAR.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G2/A_G2_LOADER.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G2/A_TIT_2014_40.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G2/A_TIT_2015_40 V2.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G3/A_G3_FINAl.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G3/A_G3_LOADER.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G3/A_TIT_40.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G3/A_TIT_40_2010.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G4/A_G4_FINAL.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G4/A_G4_LOADER.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G4/A_MAT_40 2010.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G4/BORRAR.csv
+	git reset HEAD  AppAuditMatriculas/A_CSV/G4/uno.csv
+	git reset HEAD  AppAuditMatriculas/MAT_40 2010.xls
+	git reset HEAD  AppAuditMatriculas/MAT_40 2011.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_2013_IES_40 2013.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_2014_POSGRADOYPOSTITULO_40 2014.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_2014_PREGRADO_40 2014.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_2015_POSTGRADOYPOSTITULO_40 2015.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_2015_PREGRADO_40 2015.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_40 2010.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_40 2011.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_POSTGRADO_Y_POSTITULO_2013_IES_40 2013.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_POSTGRADO_y_POSTITULO_40 2012.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/MAT_PREGRADO_40 2012.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/TIT_2014_40.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/TIT_2015_40 V2.xls
+	git reset HEAD  AppAuditMatriculas/Planillas/TIT_40.xlsx
+	git reset HEAD  AppAuditMatriculas/Planillas/TIT_40_2010.xlsx
+	git reset HEAD  AppAuditMatriculas/historial.php
+	git reset HEAD  CAE/20150914 PREPAGOS U. ARCIS.xlsx
+	git reset HEAD  CAE/INVENTARIO CAE 2015-Pamela Actualizado.xlsx
+	git reset HEAD  CAE/RENOVANTES CON DESEMBOLSO UARCIS 10-09-2015.xlsx
+
+
